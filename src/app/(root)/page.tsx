6efb4ec246c7403440cd46e63189f3e1b5851314
Josue Meza <josue_benjamin12@hotmail.com>
2024-02-01 16:59:00 -0600

    initial_commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full p-4">
+        <Button variant={"destructive"}>algo</Button>
+    </div>
+    
+  );
+}
